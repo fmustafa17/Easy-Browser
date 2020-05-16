64c63ef1bdc3a39eabf435c2ac31ec602c844fbe
Farhana Mustafa <farhana.x.mustafa@kp.org>
2020-05-16 16:16:14 -0700

    first commit

---
+++ Easy Browser/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  Easy Browser
+//
+//  Created by Farhana Mustafa on 5/16/20.
+//  Copyright © 2020 Farhana Mustafa. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate {
+
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let url = URL(string: "https://fmustafa17.github.io/")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+    }
+
+    @objc func openTapped() {
+        let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "https://www.linkedin.com/in/fmustafa/", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "https://twitter.com/farhanaxmustafa", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        ac.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem //iPad only
+        present(ac, animated: true)
+    }
+
+    func openPage(action: UIAlertAction) {
+        let url = URL(string: "https://" + action.title!)!
+        webView.load(URLRequest(url: url))
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        self.title = webView.title
+    }
+}
+
